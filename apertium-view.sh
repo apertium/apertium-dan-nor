3071ed3550511688843f4f35ed455019e1b47a7e
Francis M. Tyers <ftyers@prompsit.com>
2013-04-24 12:20:52 +0000

    update

--- apertium-view.sh
+++ apertium-view.sh
@@ -34,7 +34,7 @@ bash "$TEEMODE" $OPTION $OPTION_TAGGER >/dev/null;
 # Print all stages:
 tr '|' '\n' < "$TEEMODE" | sed "s/\$1/$OPTION/
 s/\$2/$OPTION_TAGGER/" |\
-awk -F'tee ' '# Every other line should be a tee, then a command
+gawk -F'tee ' '# Every other line should be a tee, then a command
 /tee \/tmp\/apertium-view-/ {while(("cat "$2 | getline line)>0){print line;};print ""; next}
 //                          {$0="| "$0" |"; 
                              s=sprintf("%"length($0)"s","");gsub(" ","_",s);print s; # pretty
