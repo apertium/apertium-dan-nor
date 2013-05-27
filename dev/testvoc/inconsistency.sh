67fac874280d73fb46abb4405d07b6b87bc42137
Francis M. Tyers <ftyers@prompsit.com>
2013-05-27 08:35:06 +0000

    pprs rule

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -1,18 +1,20 @@
 TMPDIR=/tmp
 
-if [[ $1 == "dan-nob" ]]; then
-lt-expand ../../apertium-da-nb.da.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+DIR=$1
+
+if [[ $DIR == "dan-nob" ]]; then
+lt-expand ../../apertium-dan-nor.dan.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../../apertium-da-nb.da-nb.t1x  ../../da-nb.t1x.bin  ../../da-nb.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc -d ../../da-nb.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
-	lt-proc -d ../../nb-da.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
+        apertium-transfer ../../apertium-dan-nor.dan-nob.t1x  ../../dan-nob.t1x.bin  ../../dan-nob.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../../dan-nob.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
+	lt-proc -d ../../nor-dan.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
 paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/ ------>  /g'
-elif [[ $1 == "nob-dan" ]]; then
-lt-expand ../../apertium-da-nb.nb.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent><clb>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+elif [[ $DIR == "nob-dan" ]]; then
+lt-expand ../../apertium-dan-nor.nob.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent><clb>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../../apertium-da-nb.nb-da.t1x  ../../nb-da.t1x.bin  ../../nb-da.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc -d ../../nb-da.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
-	lt-proc -d ../../da-nb.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
+        apertium-transfer ../../apertium-dan-nor.nob-dan.t1x  ../../nob-dan.t1x.bin  ../../nob-dan.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../../nob-dan.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
+	lt-proc -d ../../dan-nob.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
 paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/ ------>  /g'
 else
 	echo "Unsupported mode.";
