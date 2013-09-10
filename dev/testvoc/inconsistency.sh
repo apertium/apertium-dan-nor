4504e4e42202c067549413374cb750b72706f378
Francis M. Tyers <ftyers@prompsit.com>
2013-09-10 08:34:36 +0000

    update for nynorsk

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -8,12 +8,27 @@ lt-expand ../../apertium-dan-nor.dan.dix | grep -v '<prn><enc>' | grep -v 'REGEX
         lt-proc -d ../../dan-nob.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
 	lt-proc -d ../../nor-dan.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
 paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/ ------>  /g'
+
 elif [[ $DIR == "nob-dan" ]]; then
 lt-expand ../../apertium-dan-nor.nob.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent><clb>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../../apertium-dan-nor.nob-dan.t1x  ../../nob-dan.t1x.bin  ../../nob-dan.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../../nor-dan.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
 	lt-proc -d ../../dan-nob.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
+paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/ ------>  /g'
+elif [[ $DIR == "nno-dan" ]]; then
+lt-expand ../../apertium-dan-nor.nno.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent><clb>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+        apertium-pretransfer|
+        apertium-transfer ../../apertium-dan-nor.nno-dan.t1x  ../../nno-dan.t1x.bin  ../../nno-dan.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../../nor-dan.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
+	lt-proc -d ../../dan-nno.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
+paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/ ------>  /g'
+elif [[ $DIR == "dan-nno" ]]; then
+lt-expand ../../apertium-dan-nor.dan.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+        apertium-pretransfer|
+        apertium-transfer ../../apertium-dan-nor.dan-nno.t1x  ../../dan-nno.t1x.bin  ../../dan-nno.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../../dan-nno.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
+	lt-proc -d ../../nor-dan.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
 paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/ ------>  /g'
 else
 	echo "Unsupported mode.";
--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -4,7 +4,7 @@ bash inconsistency.sh nob-dan > /tmp/nob-dan.testvoc; bash inconsistency-summary
 echo ""
 
 elif [[ $1 = "nno-dan" ]]; then
-echo "==Danish->Norwegian (Bokmål)===========================";
+echo "==Norwegian (Nynorsk)->Danish===========================";
 bash inconsistency.sh nno-dan > /tmp/nno-dan.testvoc; bash inconsistency-summary.sh /tmp/nno-dan.testvoc nno-dan
 
 elif [[ $1 = "dan-nob" ]]; then
@@ -12,7 +12,7 @@ echo "==Danish->Norwegian (Bokmål)===========================";
 bash inconsistency.sh dan-nob > /tmp/dan-nob.testvoc; bash inconsistency-summary.sh /tmp/dan-nob.testvoc dan-nob
 
 elif [[ $1 = "dan-nno" ]]; then
-echo "==Danish->Norwegian (Bokmål)===========================";
+echo "==Danish->Norwegian (Nynorsk)===========================";
 bash inconsistency.sh dan-nno > /tmp/dan-nno.testvoc; bash inconsistency-summary.sh /tmp/dan-nno.testvoc dan-nno
 
 
