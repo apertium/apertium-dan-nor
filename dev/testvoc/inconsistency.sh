1b02f4802656d1a054e8c4be49c08e3347e7b681
Jonas Fromseier Mortensen <jonas@fromseier.dk>
2013-09-11 11:09:53 +0000

    fix more closed cat nno-dan, testvoc

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -12,14 +12,14 @@ paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc
 elif [[ $DIR == "nob-dan" ]]; then
 lt-expand ../../apertium-dan-nor.nob.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent><clb>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../../apertium-dan-nor.nob-dan.t1x  ../../nob-dan.t1x.bin  ../../nob-dan.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        apertium-transfer ../../apertium-dan-nor.nor-dan.t1x  ../../nor-dan.t1x.bin  ../../nob-dan.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../../nor-dan.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
 	lt-proc -d ../../dan-nob.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
 paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/ ------>  /g'
 elif [[ $DIR == "nno-dan" ]]; then
 lt-expand ../../apertium-dan-nor.nno.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -e ':<:' -e '\w:\w' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent><clb>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../../apertium-dan-nor.nno-dan.t1x  ../../nno-dan.t1x.bin  ../../nno-dan.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
+        apertium-transfer ../../apertium-dan-nor.nor-dan.t1x  ../../nor-dan.t1x.bin  ../../nno-dan.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../../nor-dan.autogen.bin  | sed 's/ \.//g' > $TMPDIR/tmp_testvoc3.txt
 	lt-proc -d ../../dan-nno.autogen.bin $TMPDIR/tmp_testvoc1.txt | sed 's/ \.//g'  > $TMPDIR/tmp_testvoc0.txt
 paste -d _ $TMPDIR/tmp_testvoc0.txt $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/ ------>  /g'
